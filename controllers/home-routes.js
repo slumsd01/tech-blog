4fdc73d78fa23a4fdc9298374ce409091eb9b33c
slumsd01 <90315740+slumsd01@users.noreply.github.com>
2022-01-29 03:13:13 -0500

    populate home page with sequelize data

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,7 +1,30 @@
 const router = require('express').Router();
+const sequelize = require('../config/connection');
+const { Post, User, Comment } = require('../models');
 
 router.get('/', (req, res) => {
-    res.render('homepage');
+    Post.findAll({
+        attributes: [
+            'id',
+            'post_body',
+            'title',
+            'created_at'
+        ],
+        include: [
+            {
+                model: User,
+                attributes: ['username']
+            }
+        ]
+    })
+    .then(dbPostData => {
+        const posts = dbPostData.map(post => post.get({ plain: true }));
+        res.render('homepage', { posts });
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
 });
 
 module.exports = router;
