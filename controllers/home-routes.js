8c4a4af7643d993732b66585ae0ef2ad1a2b3829
slumsd01 <90315740+slumsd01@users.noreply.github.com>
2022-01-29 04:08:36 -0500

    added login & logout functionality

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -2,7 +2,9 @@ const router = require('express').Router();
 const sequelize = require('../config/connection');
 const { Post, User, Comment } = require('../models');
 
+// all posts for homepage
 router.get('/', (req, res) => {
+    console.log(req.session)
     Post.findAll({
         attributes: [
             'id',
@@ -25,6 +27,21 @@ router.get('/', (req, res) => {
         console.log(err);
         res.status(500).json(err);
     });
+});
+
+// login
+router.get('/login', (req, res) => {
+    if (req.session.loggedIn) {
+        res.redirect('/');
+        return;
+    }
+  
+    res.render('login');
+});
+
+// signup
+router.get('/signup', (req, res) => {
+    res.render('signup');
 });
 
 module.exports = router;
